bb9982ca2cdb4fdaa0bc1d9ae8aa0e2397f48849
chaofei <chaofeibest@163.com>
2019-09-29 19:58:19 +0800

    增加@Quanlifier的实例告诉Spring哪些bean应当自动装配

--- spring/src/main/java/org/chaofei/entity/ACustomer.java
+++ spring/src/main/java/org/chaofei/entity/ACustomer.java
@@ -1,13 +1,19 @@
 package org.chaofei.entity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Qualifier;
+/**
+ * @author chaofei
+ *   但是，两个类似的 bean "org.chaofei.entity.APerson" 在 bean 配置文件中声明。
+ * Spring会知道哪个person bean应当自动装配？
+ *   要解决上述问题，需要使用 @Quanlifier 告诉Spring哪些bean应当自动装配。
+ */
 public class ACustomer {
-    //you want autowired this field
-    //  @Autowired 字段
-    @Autowired
+    // @Autowired 字段
     // Spring不能找到一个匹配的Bean，person属性将不设定
 //    @Autowired(required=false)
+    @Autowired
+    @Qualifier("aperson2")
     private APerson aperson;
     private int type;
     private String action;
